a97efd4e3354eff4e7564189c3e5b6ea5fc57b53
MaxUss44 <maksuss44@gmail.com>
2023-06-04 10:34:10 +0200

    renaming

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ const slider = document.querySelector(".swiper-container");
 const swiper = new Swiper(slider, {
     slidesPerView: 2,
     spaceBetween: 22,
+    preloadImages: true,
     loop: true,
     autoplay: {
         delay: 5000,
+        disableOnInteraction: false,
     },
 });
